12b9f85f1a5e5ce9416b60dde9e0ceb168b91c93
anchellon <anshulmathew@gmail.com>
2023-06-09 21:13:13 -0700

    local machine is 127.0.01

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 let config = {
-  backend_endpoint: "https://api.cloudcamp.anshul.blog",
+  // backend_endpoint: "https://api.cloudcamp.anshul.blog",
+  backend_endpoint: "http://127.0.0.1:3000",
 };
 
 export default config;
