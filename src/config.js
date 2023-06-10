b31c8d12d35a6b6cb1885a0f8aae9d6244289609
anchellon <anshulmathew@gmail.com>
2023-06-09 21:22:13 -0700

    config ficss

--- src/config.js
+++ src/config.js
@@ -1,10 +1,6 @@
 let config = {
   // backend_endpoint: "https://api.cloudcamp.anshul.blog",
-<<<<<<< HEAD
   backend_endpoint: "http://127.0.0.1:3000",
-=======
-  backend_endpoint: "http://localhost:3000",
->>>>>>> loader+context
 };
 
 export default config;
